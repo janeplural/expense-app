8e4172b7cd8c0b7ec0fc41b083d644dbaa357ac8
janeplural <mlle.jihshin@gmail.com>
2019-05-01 16:06:29 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+  },
+  mode: 'development'
+}
+// webpack config file must live in root folder
+// need entry -> output
+// entry can be a relative path
+// output must be an absolute path
+
