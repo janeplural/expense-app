f5fe2fdaa465aa715a6241fc4192de521f1f600d
janeplural <mlle.jihshin@gmail.com>
2019-05-11 14:55:07 -0400

    passed object on expense form submit

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -10,7 +10,8 @@ export default class ExpenseForm extends React.Component {
     note: '',
     amount: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   }
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -26,18 +27,33 @@ export default class ExpenseForm extends React.Component {
       this.setState(() => ({ amount }));
     }
   };
-  onDateChange = () => {
+  onDateChange = (createdAt) => {
     if (createdAt) {
       this.setState(()=>({ createdAt }));
     }
   };
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }));
-  }
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide description and amount.' }))
+    } else {
+      this.setState(() => ({ error: '' }))
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    }
+  };
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
